5aa0026ca7e5bb2437df56cd66a67ee2c55dd884
haiderkashif <kashif2019@namal.edu.pk>
2021-10-02 13:29:40 -0700

    kashi commits

---
+++ Add_Div.py
@@ -0,0 +1,17 @@
+#Addition
+
+
+def add(x,y):
+	return x + y
+
+a = add(10, 12)
+print(a)
+
+
+#division
+num1 = ("enter the first number")
+num2 = ("enter the second number")
+
+div= num1/num2
+
+print("the division of {0} and {1} is: {2}" , format(num1 , num2 , div))
