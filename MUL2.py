c749370bb2419e45588dd72877dcc1c2c342979b
azeemjutt01 <azeem2019@namal.edu.pk>
2021-10-03 14:26:51 +0500

    azeeem commited

---
+++ MUL2.py
@@ -0,0 +1,11 @@
+num1 = ("enter the first number")
+num2 = ("enter the second number")
+
+div= num1/num2
+
+print("the division of {0} and {1} is: {2}" , format(num1 , num2 , div))
+
+#azeem
+def mul(n1,n2):
+	return n1*n2
+print(mul(4,2))
