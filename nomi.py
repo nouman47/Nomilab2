f5ab400d574c3dcb9150b0d315d60f5cb05e9d02
Junaid Noor <Junaid Noor>
2021-10-03 02:33:53 -0700

    junaid commit

--- nomi.py
+++ nomi.py
@@ -4,3 +4,12 @@ num2 = ("enter the second number")
 div= num1/num2
 
 print("the division of {0} and {1} is: {2}" , format(num1 , num2 , div))
+
+
+
+# Junaid code
+
+SUB(a1,a2):
+	return a1 - a2
+x = SUB(4,6)
+print(x) 
